7666f9693962add07901c3be0fea1963417a900b
Stefan Cutovic <scuto26@uw.edu>
2026-05-03 00:46:27 -0700

    Added generator and optimizer js files

---
+++ generator.js
@@ -0,0 +1,6 @@
+// generator.js
+// Walks the optimized AST and emits JavaScript source code as the target output.
+
+export default function generate(program) {
+  throw new Error('Not implemented')
+}
---
+++ optimizer.js
@@ -0,0 +1,7 @@
+// optimizer.js
+// Transforms the analyzed AST to improve the program before code generation.
+// Performs constant folding, algebraic simplification, and dead code elimination.
+
+export default function optimize(program) {
+  throw new Error('Not implemented')
+}
